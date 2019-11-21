62059fc8e4666f5b1e02707745e63c7ab662d854
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-11-21 15:16:58 -0500

    joshua-holtsclaw

--- data/migrations/20191116140506_totalTable.js
+++ data/migrations/20191116140506_totalTable.js
@@ -33,7 +33,7 @@ exports.up = function(knex) {
         tbl.string('THC_Percent',255).notNullable();
         tbl.string('CBD',255).notNullable();
         tbl.string('Description1',255).notNullable();
-        tbl.integer('Score').notNullable();
+        tbl.float('Score').notNullable();
         tbl.integer('Recommendation');
         tbl.integer('exp');
         tbl.integer('iat');
